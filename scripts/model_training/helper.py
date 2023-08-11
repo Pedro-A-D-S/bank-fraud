d8751eafb603355ce23a24062abec45b004d7155
Pedro <pedroalves0409@gmail.com>
2023-08-11 20:56:46 -0300

    updating-helper-functions

--- scripts/model_training/helper.py
+++ scripts/model_training/helper.py
@@ -2,19 +2,34 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 import mlflow
 from sklearn.metrics import confusion_matrix, roc_auc_score
+from typing import Any
 
 
-def train(sk_model, x_train, y_train):
+def train(sk_model: Any, x_train: Any, y_train: Any) -> None:
+    """
+    Train a scikit-learn model, log train accuracy.
+    
+    Args:
+        sk_model (Any): Scikit-learn model object.
+        x_train (Any): Training input data.
+        y_train (Any): Training target data.
+    """
     sk_model = sk_model.fit(x_train, y_train)
     
     train_acc = sk_model.score(x_train, y_train)
     mlflow.log_metric('train_acc', train_acc)
     print(f'Train accuracy: {train_acc:.2%}')
 
-def evaluate(sk_model, x_test, y_test):
+def evaluate_and_plot(sk_model: Any, x_test: Any, y_test: Any) -> None:
+    """
+    Evaluate a scikit-learn model, log evaluation metrics, and plot confusion matrix.
     
+    Args:
+        sk_model (Any): Scikit-learn model object.
+        x_test (Any): Testing input data.
+        y_test (Any): Testing target data.
+    """
     eval_acc = sk_model.score(x_test, y_test)
-    
     preds = sk_model.predict(x_test)
     auc_score = roc_auc_score(y_test, preds)
     
@@ -25,12 +40,26 @@ def evaluate(sk_model, x_test, y_test):
     print(f'AUC score: {auc_score:.2%}')
     
     conf_matrix = confusion_matrix(y_test, preds)
-    ax = sns.heatmap(conf_matrix, annot = True, fmt = 'g')
+    plot_confusion_matrix(conf_matrix)
+    save_confusion_matrix_plot()
+
+def plot_confusion_matrix(conf_matrix: Any) -> None:
+    """
+    Plot a confusion matrix using seaborn.
+    
+    Args:
+        conf_matrix (Any): Confusion matrix data.
+    """
+    ax = sns.heatmap(conf_matrix, annot=True, fmt='g')
     ax.invert_xaxis()
     ax.invert_yaxis()
     plt.xlabel('Predicted')
     plt.ylabel('Actual')
     plt.title('Confusion Matrix')
-    plt.savefig('sklearn_conf_matrix.png')
     
+def save_confusion_matrix_plot() -> None:
+    """
+    Save the confusion matrix plot as an artifact.
+    """
+    plt.savefig('sklearn_conf_matrix.png')
     mlflow.log_artifact('sklearn_conf_matrix.png')
--- scripts/model_training/model_training.py
+++ scripts/model_training/model_training.py
@@ -12,6 +12,10 @@ import logging
 import mlflow
 import mlflow.sklearn
 import os
+from helper import (
+    train,
+    evaluate
+)
 
 # Set up
 logging.basicConfig(filename = './logs/data_prep_training.log', level = logging.INFO,
@@ -22,37 +26,6 @@ mlflow_tracking_uri = './data/mlflow/mlruns'
 
 # Set the environment variable
 os.environ['MLFLOW_TRACKING_URI'] = mlflow_tracking_uri
-
-# functions
-def train(sk_model, x_train, y_train):
-    sk_model = sk_model.fit(x_train, y_train)
-    
-    train_acc = sk_model.score(x_train, y_train)
-    mlflow.log_metric('train_acc', train_acc)
-    print(f'Train accuracy: {train_acc:.2%}')
-
-def evaluate(sk_model, x_test, y_test):
-    eval_acc = sk_model.score(x_test, y_test)
-    
-    preds = sk_model.predict(x_test)
-    auc_score = roc_auc_score(y_test, preds)
-    
-    mlflow.log_metric('eval_acc', eval_acc)
-    mlflow.log_metric('auc_score', auc_score)
-    
-    print(f'Test accuracy: {eval_acc:.2%}')
-    print(f'AUC score: {auc_score:.2%}')
-    
-    conf_matrix = confusion_matrix(y_test, preds)
-    ax = sns.heatmap(conf_matrix, annot=True, fmt='g')
-    ax.invert_xaxis()
-    ax.invert_yaxis()
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-    plt.title('Confusion Matrix')
-    plt.savefig('sklearn_conf_matrix.png')
-    
-    mlflow.log_artifact('sklearn_conf_matrix.png')
 
 # Log the start of the script
 logging.info('Data preprocessing script started.')
